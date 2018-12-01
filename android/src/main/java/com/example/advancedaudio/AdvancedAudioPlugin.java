dc40556e408bacb36bcb643d5112f31c1798d38b
Randi Miller <randi@geekguy.co>
2018-12-01 01:11:31 -0500

    Android audio channel methods

--- android/src/main/java/com/example/advancedaudio/AdvancedAudioPlugin.java
+++ android/src/main/java/com/example/advancedaudio/AdvancedAudioPlugin.java
@@ -1,16 +1,23 @@
 package com.example.advancedaudio;
 
+import java.io.IOException;
+
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+
 /** AdvancedAudioPlugin */
 public class AdvancedAudioPlugin implements MethodCallHandler {
+  MediaPlayer mediaPlayer;
+
   /** Plugin registration. */
   public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "advanced_audio");
+    final MethodChannel channel = new MethodChannel(registrar.messenger(), "podl.io/advanced_audio");
     channel.setMethodCallHandler(new AdvancedAudioPlugin());
   }
 
@@ -18,8 +25,45 @@ public class AdvancedAudioPlugin implements MethodCallHandler {
   public void onMethodCall(MethodCall call, Result result) {
     if (call.method.equals("getPlatformVersion")) {
       result.success("Android " + android.os.Build.VERSION.RELEASE);
+    } else if (call.method.equals("play")) {
+      String url = call.argument("url");
+      play(url);
+      result.success(null);
+    } else if (call.method.equals("pause")) {
+      pause();
+    } else if (call.method.equals("stop")) {
+      stop();
     } else {
       result.notImplemented();
     }
+  }
+
+  private void pause() {
+    mediaPlayer.pause();
+  }
+
+  private void stop() {
+    mediaPlayer.stop();
+    mediaPlayer.release();
+    mediaPlayer = null;
+  }
+
+  private void play(String url) {
+    if (mediaPlayer == null) {
+      mediaPlayer = new MediaPlayer();
+      mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+      try {
+        mediaPlayer.setDataSource(url);
+        mediaPlayer.prepare();
+      } catch (IOException e) {
+        // TODO: handle exception.. this should send something back to the app
+      }
+
+      mediaPlayer.start();
+    } else {
+      mediaPlayer.start();
+    }
+
   }
 }
